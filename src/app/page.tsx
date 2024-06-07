a6740d25829b46bba1dc31f05cb1e7324ddb248b
Abel Bodogan <bodoganabel@gmail.com>
2024-06-08 00:28:17 +0200

    bugfix - white text to black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import HomepageSearch from "@/components/HomepageSearch";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-black w-full">Hello</div>
+    <div className="text-black w-full"><HomepageSearch/></div>
   );
 }
